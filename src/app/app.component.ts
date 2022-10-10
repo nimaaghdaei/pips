a8f39233f4d0b71bba2c37945f5bec8537dd6173
Nima <Nima.aghdaei@gmail.com>
2022-10-10 10:29:01 +0330

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pips';
+  name :string='';
+  date :string='';
+
+  ONchangename(value:string){
+    this.name = value;
+  }
+  ONchangedate(value:string){
+    this.date = value;
+  }
 }
